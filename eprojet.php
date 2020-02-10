0e6f25766ada067ade4018e8ac054281ae9faf91
Mikaïdou M'Bo <mikaidoumbo@hotmail.fr>
2020-02-10 21:45:01 +0100

    initialize

---
+++ eprojet.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <style>
+        .conteneur {
+            width: 1000px;
+            margin: 0 auto;
+        }
+
+        form {
+            width: 50%;
+            padding: 20px;
+            border: 1px solid #333;
+            margin: 0 auto;
+        }
+
+        input,
+        select,
+        textarea {
+            width: 100%;
+            border: 1px solid #333;
+            min-height: 30px;
+            resize: vertical;
+        }
+
+        #confirm {
+            background-color: #333;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+
+
+
+    <div class="conteneur"></div>
+    <?php
+
+
+
+    // echo '<pre>'; var_dump($_POST); echo '</pre>';
+ 
+    for($i=0;$i<=100;$i++)
+    {
+        
+    }
+    
+    ?>
+    <form method="post" action="affichage_exercice2.php" enctype="multipart/form-data">
+
+        <h1>Formulaire membre</h1>
+        <label for="nom">Nom</label>
+        <input type="nom" name="nom" id="nom">
+        <br>
+        <label for="prenom">Prenom</label>
+        <input type="prenom" name="prenom" id="prenom">
+
+        <label for="adresse">Adresse</label>
+        <textarea name="adresse" id="adresse"></textarea>
+        <hr>
+        
+        <label for="ville">ville</label>
+        <input type="ville" name="ville" id="ville">
+        <hr>
+        <label for="cp">Code postal</label>
+        <input type="cp" name="cp" id="cp">
+        <hr>
+        <label for="description">Description</label>
+        <textarea class="form-control" name="description" rows="10"></textarea>
+        <hr>
+        <input type="submit" id="confirm" value="Confirmer">
+    </form>
+
+    </div>
+</body>
+
+</html>
